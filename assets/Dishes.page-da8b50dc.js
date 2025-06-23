aaac9ef8154cd6be12ecdf0fb57b61b75bae349a
Hzd <205839042+Hzd-Gaming@users.noreply.github.com>
2025-06-23 09:14:49 +0700

    Updates

---
+++ assets/Dishes.page-da8b50dc.js
@@ -0,0 +1,2 @@
+import{j as e,C as a}from"./index-ee5e6df8.js";import{u as t}from"./index-e5d70896.js";const i=()=>{const s=t("(min-width: 768px)");return e.jsx(a,{className:"dishes_page",...s?{}:{titlePage:"DISHES"}})};export{i as default};
+//# sourceMappingURL=Dishes.page-da8b50dc.js.map
